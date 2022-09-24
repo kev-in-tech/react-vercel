01f5b07fcc1c0b3d7e155f3ca9723ed4a627bc82
Gabor Kevin Barta <gkevinba@gmail.com>
2022-09-24 17:33:31 +0200

    minor change to main page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          We are deploying this website to Vercel
         </p>
         <a
           className="App-link"
